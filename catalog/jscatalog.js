d4ec0f0937f5eabb86fff854feaaa3b8674d5015
fantom4ik55 <80359534+fantom4ik55@users.noreply.github.com>
2024-10-08 18:25:14 +0300

    Add files via upload

---
+++ catalog/jscatalog.js
@@ -0,0 +1,266 @@
+const dataCatalog = `[
+    {
+    "img_card": "./img/Rectangle1.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "262"
+     },
+     {
+     "img_card": "./img/Image.jpg",
+      "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "262"
+     },
+     {
+       "img_card": "./img/product2.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/product1.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/Image2.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image3.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+    
+     {
+       "img_card": "./img/Image4.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+      "img_card": "./img/Image5.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image6.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/product1.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/Image2.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image3.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+    
+     {
+       "img_card": "./img/Image4.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+      "img_card": "./img/Image5.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image6.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+     "img_card": "./img/Image.jpg",
+      "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "262"
+     },
+     {
+       "img_card": "./img/product2.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/product1.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/Image2.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image3.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+    
+     {
+       "img_card": "./img/Image4.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+      "img_card": "./img/Image5.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image6.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/product1.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+        "img_card": "./img/Image2.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image3.jpg",
+       "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+    
+     {
+       "img_card": "./img/Image4.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+      "img_card": "./img/Image5.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     },
+     {
+       "img_card": "./img/Image6.jpg",
+        "title_card": "ELLERY X M'O CAPSULE",
+       "text_card": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+       "price_card": "42" 
+     }
+    ]`;
+
+const catalogBox = document.querySelector('.product_box');
+const cardsContainer = document.createElement('div');
+const paginationEl = document.createElement('div');
+paginationEl.classList.add('product_box__pagination');
+cardsContainer.classList.add('product_box-contener');
+catalogBox.appendChild(cardsContainer);
+catalogBox.appendChild(paginationEl);
+const anchorEl = document.createElement('a');
+
+
+
+const dataCatalogs = JSON.parse(dataCatalog);
+let cardsPerPage = 9;
+let currentPage = 1;
+
+function displayCards(arrData, rows, page) {
+  cardsContainer.innerHTML = '';
+  page--;
+  const start = rows * page;
+  const end = start + rows;
+  const paginatedData = arrData.slice(start, end);
+
+  paginatedData.forEach((element) => {
+    const dataCart = `
+          <div class="product">
+            <img class="product_img" src="${element.img_card}" alt="product">
+            <div class="product_content">
+              <h3 class="product_name"> ${element.title_card}</h3>
+              <p class="product_text">${element.text_card}</p>
+              <p class="product_price">$ ${element.price_card}</p>
+            </div>
+          </div>
+        `;
+    cardsContainer.insertAdjacentHTML('beforeend', dataCart);
+  });
+}
+displayCards(dataCatalogs, cardsPerPage, currentPage);
+
+function displayPagination(arrData, rowPerPage) {
+  paginationEl.innerHTML = '';
+  const pagesCount = Math.ceil(arrData.length / rowPerPage);
+  const ulEl = document.createElement('ul');
+  ulEl.classList.add('product_box__pagination-list');
+
+  for (let i = 0; i < pagesCount; i++) {
+    const liEl = displayPaginationBtn(i + 1);
+    ulEl.appendChild(liEl);
+  }
+  paginationEl.appendChild(ulEl);
+}
+displayPagination(dataCatalogs, cardsPerPage);
+
+function displayPaginationBtn(page) {
+  const liEl = document.createElement('li');
+  liEl.classList.add('product_box__pagination-item');
+  liEl.innerText = page;
+  if (currentPage == page) liEl.classList.add('product_box__pagination-active');
+
+  liEl.addEventListener('click', () => {
+    currentPage = page;
+    displayCards(dataCatalogs, cardsPerPage, currentPage);
+
+    let currentitemLi = document.querySelector('li.product_box__pagination-active');
+    currentitemLi.classList.remove('product_box__pagination-active');
+    liEl.classList.add('product_box__pagination-active');
+
+    const linkEl = document.createElement('a');
+    linkEl.href = '#section';
+
+    const ancorEl = document.querySelector('.header');
+    ancorEl.scrollIntoView({ behavior: "smooth" });
+  });
+
+  return liEl;
+}
+
+
+
+
+
+
+
+
+
+
